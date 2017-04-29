6c3c98d3df96c6660402627f30eed05bbb759aaf
Souvik Basu <souvikbasu@gmail.com>
2017-04-29 16:00:55 +0530

    Header and Landing page components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderBarComponent } from './header-bar/header-bar.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderBarComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
